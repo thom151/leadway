acd8c0882f9892a393914b249e4e8893a001f066
thom151 <thomassantos2003@gmail.com>
2025-04-09 21:43:41 +1000

    formatted code

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -74,14 +74,13 @@ func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 }
 
 func GetBearerToken(headers http.Header, cookies []*http.Cookie) (string, error) {
-	
-		for _, cookie := range cookies {
-			log.Printf("cookie name: %v\n", cookie.Name)
-			if cookie.Name == "acc_token" {
-				return cookie.Value, nil
-			}
-		}
 
+	for _, cookie := range cookies {
+		log.Printf("cookie name: %v\n", cookie.Name)
+		if cookie.Name == "acc_token" {
+			return cookie.Value, nil
+		}
+	}
 
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
